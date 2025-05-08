e70981fc38f69f02a8fa042c362cc5d8b764f75f
Stefan Zwanenburg <stefan+git@zwanenburg.info>
2025-05-08 21:26:43 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use traefik_wasm_api as traefik;
+
+#[unsafe(export_name = "handle_request")]
+fn http_request() -> i64 {
+    let request_uri = traefik::get_request_uri();
+    let mut url = url::Url::parse(&request_uri).unwrap();
+    url.query_pairs_mut()
+        .append_pair("server", "https://matrix.zwanenburg.info");
+    traefik::set_request_uri(url.as_str());
+
+    1
+}
+
+fn main() {}
